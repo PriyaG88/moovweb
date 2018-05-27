3667043ad7a6fffb809cc4e47705592ca4f96eb4
Priya Gurung <priyaprograms@gmail.com>
2018-05-26 17:20:47 -0700

    Add category, product and cart routes

--- scripts/routes.js
+++ scripts/routes.js
@@ -12,11 +12,28 @@ module.exports = function() {
   // 3. Required. `imports` [filename paths, array] OR `passthrough` [string of
   //  extra data for conditional passthrough, such as "perfectProxy"]
 
-  routes.mappings.push({
-    name: "home",
-    url: /^\/($|\?)/,
-    imports: ["/pages/home.js"]
-  });
+  routes.mappings.push(
+    {
+      name: "home",
+      url: /^\/($|\?)/,
+      imports: ["/pages/home.js"]
+    },
+    {
+      name: "category",
+      url: /shop/,
+      imports: ["/pages/category.js"]
+    },
+    {
+      name: "product",
+      url: /sample-product/,
+      imports: ["/pages/product.js"]
+    },
+    {
+      name: "cart",
+      element: "#CartContent",
+      imports: ["/pages/cart.js"]
+    }
+  );
 
   return routes;
 };
